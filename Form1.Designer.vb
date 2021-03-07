4850c4556b39d7fc872eddc8f66ad9666deedcfb
Zachary Childers <zdchilders10@sccsc.edu>
2021-03-06 22:52:37 -0500

    Commit for big pc

---
+++ Form1.Designer.vb
@@ -0,0 +1,268 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GrpOne = New System.Windows.Forms.GroupBox()
+        Me.GrpTwo = New System.Windows.Forms.GroupBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.ListBox3 = New System.Windows.Forms.ListBox()
+        Me.ListBox4 = New System.Windows.Forms.ListBox()
+        Me.ListBox5 = New System.Windows.Forms.ListBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.BtnSales_Names = New System.Windows.Forms.Button()
+        Me.BtnDispSales = New System.Windows.Forms.Button()
+        Me.BtnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblL = New System.Windows.Forms.Label()
+        Me.Lbl = New System.Windows.Forms.Label()
+        Me.lblr = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.GrpOne.SuspendLayout()
+        Me.GrpTwo.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GrpOne
+        '
+        Me.GrpOne.Controls.Add(Me.lblL)
+        Me.GrpOne.Controls.Add(Me.Label2)
+        Me.GrpOne.Controls.Add(Me.Label1)
+        Me.GrpOne.Controls.Add(Me.BtnDispSales)
+        Me.GrpOne.Controls.Add(Me.TextBox1)
+        Me.GrpOne.Controls.Add(Me.ListBox2)
+        Me.GrpOne.Controls.Add(Me.ListBox1)
+        Me.GrpOne.Location = New System.Drawing.Point(29, 30)
+        Me.GrpOne.Name = "GrpOne"
+        Me.GrpOne.Size = New System.Drawing.Size(340, 298)
+        Me.GrpOne.TabIndex = 0
+        Me.GrpOne.TabStop = False
+        '
+        'GrpTwo
+        '
+        Me.GrpTwo.Controls.Add(Me.Label7)
+        Me.GrpTwo.Controls.Add(Me.Label6)
+        Me.GrpTwo.Controls.Add(Me.lblr)
+        Me.GrpTwo.Controls.Add(Me.Lbl)
+        Me.GrpTwo.Controls.Add(Me.BtnSales_Names)
+        Me.GrpTwo.Controls.Add(Me.TextBox2)
+        Me.GrpTwo.Controls.Add(Me.ListBox5)
+        Me.GrpTwo.Controls.Add(Me.ListBox4)
+        Me.GrpTwo.Controls.Add(Me.ListBox3)
+        Me.GrpTwo.Location = New System.Drawing.Point(392, 29)
+        Me.GrpTwo.Name = "GrpTwo"
+        Me.GrpTwo.Size = New System.Drawing.Size(399, 298)
+        Me.GrpTwo.TabIndex = 1
+        Me.GrpTwo.TabStop = False
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 25
+        Me.ListBox1.Location = New System.Drawing.Point(15, 49)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(124, 154)
+        Me.ListBox1.TabIndex = 0
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 25
+        Me.ListBox2.Location = New System.Drawing.Point(200, 49)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(95, 79)
+        Me.ListBox2.TabIndex = 1
+        '
+        'ListBox3
+        '
+        Me.ListBox3.FormattingEnabled = True
+        Me.ListBox3.ItemHeight = 25
+        Me.ListBox3.Location = New System.Drawing.Point(105, 50)
+        Me.ListBox3.Name = "ListBox3"
+        Me.ListBox3.Size = New System.Drawing.Size(79, 54)
+        Me.ListBox3.TabIndex = 2
+        '
+        'ListBox4
+        '
+        Me.ListBox4.FormattingEnabled = True
+        Me.ListBox4.ItemHeight = 25
+        Me.ListBox4.Location = New System.Drawing.Point(6, 50)
+        Me.ListBox4.Name = "ListBox4"
+        Me.ListBox4.Size = New System.Drawing.Size(64, 54)
+        Me.ListBox4.TabIndex = 3
+        '
+        'ListBox5
+        '
+        Me.ListBox5.FormattingEnabled = True
+        Me.ListBox5.ItemHeight = 25
+        Me.ListBox5.Location = New System.Drawing.Point(164, 132)
+        Me.ListBox5.Name = "ListBox5"
+        Me.ListBox5.Size = New System.Drawing.Size(164, 154)
+        Me.ListBox5.TabIndex = 4
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(15, 236)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(150, 31)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(0, 184)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(150, 31)
+        Me.TextBox2.TabIndex = 5
+        '
+        'BtnSales_Names
+        '
+        Me.BtnSales_Names.Location = New System.Drawing.Point(216, 30)
+        Me.BtnSales_Names.Name = "BtnSales_Names"
+        Me.BtnSales_Names.Size = New System.Drawing.Size(164, 34)
+        Me.BtnSales_Names.TabIndex = 6
+        Me.BtnSales_Names.Text = "Sales and &names"
+        Me.BtnSales_Names.UseVisualStyleBackColor = True
+        '
+        'BtnDispSales
+        '
+        Me.BtnDispSales.Location = New System.Drawing.Point(200, 233)
+        Me.BtnDispSales.Name = "BtnDispSales"
+        Me.BtnDispSales.Size = New System.Drawing.Size(130, 34)
+        Me.BtnDispSales.TabIndex = 7
+        Me.BtnDispSales.Text = "&Display Sales"
+        Me.BtnDispSales.UseVisualStyleBackColor = True
+        '
+        'BtnExit
+        '
+        Me.BtnExit.Location = New System.Drawing.Point(676, 404)
+        Me.BtnExit.Name = "BtnExit"
+        Me.BtnExit.Size = New System.Drawing.Size(112, 34)
+        Me.BtnExit.TabIndex = 7
+        Me.BtnExit.Text = "E&xit"
+        Me.BtnExit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        Me.BtnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(15, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(107, 25)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Salespeople"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(200, 21)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(63, 25)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Label2"
+        '
+        'lblL
+        '
+        Me.lblL.AutoSize = True
+        Me.lblL.Location = New System.Drawing.Point(15, 208)
+        Me.lblL.Name = "lblL"
+        Me.lblL.Size = New System.Drawing.Size(56, 25)
+        Me.lblL.TabIndex = 10
+        Me.lblL.Text = "Sales:"
+        '
+        'Lbl
+        '
+        Me.Lbl.AutoSize = True
+        Me.Lbl.Location = New System.Drawing.Point(164, 107)
+        Me.Lbl.Name = "Lbl"
+        Me.Lbl.Size = New System.Drawing.Size(76, 25)
+        Me.Lbl.TabIndex = 9
+        Me.Lbl.Text = "Sold By:"
+        '
+        'lblr
+        '
+        Me.lblr.AutoSize = True
+        Me.lblr.Location = New System.Drawing.Point(0, 156)
+        Me.lblr.Name = "lblr"
+        Me.lblr.Size = New System.Drawing.Size(56, 25)
+        Me.lblr.TabIndex = 10
+        Me.lblr.Text = "Sales:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(6, 22)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(52, 25)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Years"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(105, 22)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(60, 25)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "Status"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.BtnExit)
+        Me.Controls.Add(Me.GrpTwo)
+        Me.Controls.Add(Me.GrpOne)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Adaline Inc."
+        Me.GrpOne.ResumeLayout(False)
+        Me.GrpOne.PerformLayout()
+        Me.GrpTwo.ResumeLayout(False)
+        Me.GrpTwo.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GrpOne As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents BtnDispSales As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents GrpTwo As GroupBox
+    Friend WithEvents BtnSales_Names As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents ListBox5 As ListBox
+    Friend WithEvents ListBox4 As ListBox
+    Friend WithEvents ListBox3 As ListBox
+    Friend WithEvents BtnExit As Button
+    Friend WithEvents lblL As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents lblr As Label
+    Friend WithEvents Lbl As Label
+End Class
